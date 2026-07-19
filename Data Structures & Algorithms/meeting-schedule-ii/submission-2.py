5629e8b99d5748f5e937ec6ad06f136646b152f1
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2026-07-19 07:32:40 -0700

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        # algo: intervals and use free rooms
+        # TC: O(nlogn)
+        # SC: O(n)
+        
+        if not intervals:
+            return 0
+        intervals.sort(key=lambda x:x.start)
+        rooms = []
+        heapq.heappush(rooms, intervals[0].end)
+
+        for i in range(1, len(intervals)):
+            if intervals[i].start >= rooms[0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms, intervals[i].end)
+
+        return len(rooms)
