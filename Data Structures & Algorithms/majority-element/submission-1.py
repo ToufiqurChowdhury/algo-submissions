1106e3759e7ff0d5c1698c7296d82bc7177f614e
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2026-08-17 00:03:35 -0700

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums) // 2
+
+        candidate = nums[n]
+
+        return candidate
+        
