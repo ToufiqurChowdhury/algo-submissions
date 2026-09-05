acf272b9c56bc00de61e5dd7acc36ace1e1dddad
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2026-09-04 23:54:15 -0700

    Add: swim-in-rising-water - submission-3

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        """
+            find smallest max height
+        """
+        N = len(grid)
+        visit = set()
+        minH = [[grid[0][0], 0, 0]]
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        visit.add((0, 0))
+        while minH:
+            t, r, c = heapq.heappop(minH)
+
+            if r == N-1 and c == N-1:
+                return t
+
+            for dr, dc in directions:
+                neir, neic = dr + r, dc + c
+                
+                if  neir<0 or neic<0 or neir == N or neic == N or (neir, neic) in visit:
+                    continue
+
+                visit.add((neir, neic))
+                heapq.heappush(minH, [max(t, grid[neir][neic]), neir, neic])         
