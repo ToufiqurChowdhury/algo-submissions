9959cf687f945f0bf1f957dca5f4b1c44e0ad3e8
ToufiqurChowdhury <60831585+ToufiqurChowdhury@users.noreply.github.com>
2026-09-06 21:44:00 -0700

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # algo: backtrack
+        # TC: O(n * 2^n)
+        # SC: O(n)
+
+        res, sol = [], []
+
+        def backtrack(i):
+            if i == len(nums):
+                res.append(sol[:])
+                return
+
+            # backtrack with num[i]
+            sol.append(nums[i])
+            backtrack(i+1)
+
+            # backtrack without 
+            sol.pop()
+            backtrack(i+1)
+
+        backtrack(0)
+        return res
